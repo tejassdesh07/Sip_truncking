80f4d001ef6fe67f21d1c5e80a0dd5778df2ba70
tejassdesh07 <tejasdesh01@gmail.com>
2024-08-20 04:07:00 -0700

    Created google oauth login functionality.

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const path = require('path');
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK with environment variables
-const serviceAccount = require('./secrets/serviceAccountKey.json'); // Path to your service account key
+// const serviceAccount = require('./secrets/serviceAccountKey.json'); // Path to your service account key
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  };
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
